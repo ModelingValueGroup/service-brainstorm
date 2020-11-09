450cca3975dc079a74be6b8699d2ceb1ca4f2aa5
WimBast <wim.bast@gmail.com>
2020-11-09 14:27:58 +0100

    legs

--- helloworld/src/template/Person.java
+++ helloworld/src/template/Person.java
@@ -1,5 +1,14 @@
 package template;
 
+import org.modelingvalue.collections.List;
+import org.modelingvalue.dclare.Constant;
+
 public class Person {
+
+    private final Constant<Person, List<Leg>> LEGS = Constant.of("LEGS", List.of());
+
+    public List<Leg> getLegs() {
+        return LEGS.get(this);
+    }
 
 }
