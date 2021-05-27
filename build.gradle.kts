6a63ea357ebd264b8a6bb606ff4900ae4a8bd9d0
Tom Brus <tombrus@gmail.com>
2021-05-27 22:32:46 +0200

    manual adjustments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,34 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (C) Copyright 2018-2021 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
+//                                                                                                                     ~
+// Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
+// compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+// an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// specific language governing permissions and limitations under the License.                                          ~
+//                                                                                                                     ~
+// Maintainers:                                                                                                        ~
+//     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
+// Contributors:                                                                                                       ~
+//     Arjan Kok, Carel Bast                                                                                           ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "0.4.26"
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.modelingvalue.gradle.mvgplugin") version "0.4.28" // must be after io.spring.dependency-management!!!
 }
 dependencies {
-    
-    
-    //testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    implementation("org.modelingvalue:dclare:1.4.0-BRANCHED")
-    implementation("org.modelingvalue:immutable-collections:1.4.0-BRANCHED")
+    implementation("org.modelingvalue:dclare:1.5.0-BRANCHED")
+    implementation("org.modelingvalue:immutable-collections:1.5.0-BRANCHED")
     implementation("org.modelingvalue:mvgjson:1.1.6-BRANCHED")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 publishing {
     publications {
